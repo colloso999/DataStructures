8fcd037df3f3fbab2c44bc84b050aaec9a51dda5
colloso999 <tripperskripper@gmail.com>
2023-01-11 08:24:22 +0300

    deletes an element

---
+++ BasicOperations/deletion/1_deletion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int LA[] = {1,3,5,7,8};
+  int k = 3, n = 5;
+  int i, j;
+
+  printf("The original array elements are :\n");
+
+  for (i = 0; i < n; i++)
+  {
+    printf("LA[%d] = %d\n")
+  }
+}
