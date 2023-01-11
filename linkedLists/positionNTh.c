f54a8ebfdeee1e56880713046af20e95143ee687
colloso999 <tripperskripper@gmail.com>
2023-01-11 11:17:04 +0300

    returns the position of a node

---
+++ linkedLists/positionNTh.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * insert_nodeint_at_index - insert a new node with a given integer value(n) at a
+ * specified position(idx) in a singly linked list
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+    /**
+     * new node - will be used to hold the pointer of the new node that's being inserted
+     * current - will be used to hold the current element in the list when iterating through it
+     */
+    listint_t *new_node, *current;
+    unsigned int i;
+    /* first checks if the passed head pointer is equal to null */
+    if (head == NULL)   
+        return (-1);
+
+    if (idx == 0)
+    {
+        new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL) 
+            return (-1);
+        /**
+         * * assigns the integer value passed as the second arg of the function 'n' to the field 'n'
+         * of the struct that new_node is pointing to.
+         * new_node: a pointer to a struct listint_that has been allocated memory using the malloc
+         * ->: this opertor allows access to the field of struct thru a pointer
+         *  the value is stored in the newly created node and will be used as the value of the node
+         *  when it gets inserted into the list
+         */
+        new_node->n = n;
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+    current = *head;
+    for (i = 0; i < (index - 1); i++)
+    {
+        current = current->next;
+    }
+    if (current == NULL)
+    {
+        return (NULL);
+    }
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+    new_node->n = n;
+    new_node->next = current->next;
+    current->next = new_node;
+    return (new_node);
+}
